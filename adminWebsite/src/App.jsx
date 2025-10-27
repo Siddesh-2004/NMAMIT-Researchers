0dabb2c44b4448cd40b60194da37a8d9085ccd02
siddesh <siddeshpoojary2004@gmail.com>
2025-10-27 08:57:21 +0530

    Routing Completed

--- adminWebsite/src/App.jsx
+++ adminWebsite/src/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 import Login from './pages/Login.jsx'
-import NavBar from './components/NavBar.jsx'
 import Home from './pages/Home.jsx'
 import InRevision from './pages/InRevision.jsx'
 import InReview from './pages/InReview.jsx'
-import Topic from './pages/Topic.jsx'
 import AddReviewer from './pages/AddReviewer.jsx'
 import DeleteContent from './pages/DeleteReviewer.jsx'
+import Layout from "./Layout.jsx"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const router =createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout/>}>
+        <Route path="" element={<Home/>}/>
+        <Route path='InRevision' element={<InRevision/>}/>
+        <Route path='InReview' element={<InReview/>}/>
+        <Route path='AddReviewer' element={<AddReviewer/>}/>
+        <Route path='DeleteContent' element={<DeleteContent/>}/>
+      </Route>
+    )
+  )
   return (
     <>
-      <NavBar />
-       {/* <Login />  */}
-       <Home />
-       {/* <InRevision/> */}
-       {/* <InReview/> */}
-       {/* <Topic/> */}
-       {/* <AddReviewer/> */}
-       {/* <DeleteContent/> */}
+      {
+        isLoggedIn ? <RouterProvider router={router}/> : <Login setIsLoggedIn={setIsLoggedIn}/>
+      }
     </>
   )
 }
