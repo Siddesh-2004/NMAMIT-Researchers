f6f3899348126927f53139a1e8c872591daa7d33
siddesh <siddeshpoojary2004@gmail.com>
2025-11-01 17:35:31 +0530

    Added models

--- backend/src/models/reviewer.model.js
+++ backend/src/models/reviewer.model.js
@@ -1,4 +1,17 @@
 import mongoose from 'mongoose';
-const reviewerSchema = new mongoose.Schema({});
+const reviewerSchema = new mongoose.Schema({
+    reviwerName:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+    },
+    qualification:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+    },
+});
 const ReviewerModel = mongoose.model('Reviewer', reviewerSchema);
 export default ReviewerModel;
--- backend/src/models/team.model.js
+++ backend/src/models/team.model.js
@@ -1,4 +1,16 @@
 import mongoose  from "mongoose";
-const teamSchema = new mongoose.Schema({});
+const teamSchema = new mongoose.Schema({
+    authorsIds:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    }],
+    paperId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Paper',
+        required:true,
+    },
+    
+});
 const TeamModel = mongoose.model('Team', teamSchema);
 export default TeamModel;
