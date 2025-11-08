10e10da7814a20178f18d8cef3622ab0dde43211
siddesh <siddeshpoojary2004@gmail.com>
2025-11-08 10:02:03 +0530

    added display papers functionality

--- backend/src/routes/paper.route.js
+++ backend/src/routes/paper.route.js
@@ -1,6 +1,20 @@
 import Router from "express";
 import {upload} from "../middlewares/multer.js";
-import { addPaper } from "../controllers/paper.controller.js";
+import { addPaper,getInReviewPapers,getAcceptedPapers,getInRevisionPapers,updateAcceptanceStatus,deletePaper} from "../controllers/paper.controller.js";
+
+
+
+
 const paperRoutes = Router();
 paperRoutes.post("/add",upload.single("paper"),addPaper);
+paperRoutes.get("/getInReviewPapers",getInReviewPapers);
+paperRoutes.get("/getAccepted",getAcceptedPapers);
+paperRoutes.get("/getInRevision",getInRevisionPapers);
+paperRoutes.put("/updateAcceptanceStatus",updateAcceptanceStatus);
+paperRoutes.delete("/delete",deletePaper);
+
+
+
+
+
 export default paperRoutes;
