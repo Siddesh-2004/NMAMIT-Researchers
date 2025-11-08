005d376187ed9d5b08c7c8b45568a18ed8869bfc
siddesh <siddeshpoojary2004@gmail.com>
2025-11-08 17:54:38 +0530

    added getReviewPapers functionality and find user

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { signUpUser ,loginUser,logoutUser ,verifyJwtUser} from "../controllers/user.controller.js";
+import { signUpUser ,loginUser,logoutUser ,verifyJwtUser,getUserByUserName} from "../controllers/user.controller.js";
 
 const router=Router();
 router.post("/signUp",signUpUser);
 router.post("/login",loginUser);
 router.post("/logout",logoutUser);
 router.get("/verifyJwt",verifyJwtUser);
+router.post("/findByUserName",getUserByUserName);
 export default router;
