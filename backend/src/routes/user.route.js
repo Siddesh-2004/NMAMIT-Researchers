90de780bb3ba4d118d336968fae3cafea6fc598d
siddesh <siddeshpoojary2004@gmail.com>
2025-11-09 12:20:45 +0530

    add paper functionality completed

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { signUpUser ,loginUser,logoutUser ,verifyJwtUser,getUserByUserName} from "../controllers/user.controller.js";
+import { signUpUser ,loginUser,logoutUser ,verifyJwtUser,getUserByUserName,getCurrentUser} from "../controllers/user.controller.js";
 
 const router=Router();
 router.post("/signUp",signUpUser);
@@ -7,4 +7,5 @@ router.post("/login",loginUser);
 router.post("/logout",logoutUser);
 router.get("/verifyJwt",verifyJwtUser);
 router.post("/findByUserName",getUserByUserName);
+router.get("/getCurrentUser",getCurrentUser);
 export default router;
