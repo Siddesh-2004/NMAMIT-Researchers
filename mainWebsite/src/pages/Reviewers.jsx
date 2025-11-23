16393cf245c5ec8d956115ccd19dadf4ab3407bb
Sowjan <sowjanspoojari23@gmail.com>
2025-11-23 20:15:37 +0530

    minor fix

--- mainWebsite/src/pages/Reviewers.jsx
+++ mainWebsite/src/pages/Reviewers.jsx
@@ -133,8 +133,8 @@ export default function Reviewers() {
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
                   <option value="">None</option>
-                  <option value="highest">Highest to Lowest</option>
-                  <option value="lowest">Lowest to Highest</option>
+                  <option value="highest">Highest </option>
+                  <option value="lowest">Lowest </option>
                 </select>
               </div>
               <div>
